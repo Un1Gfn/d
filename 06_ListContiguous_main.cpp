7bf0d0d156ff42754fb31c9f7ee33ada67cdc5fa
Darren Wu <darren19970810@gmail.com>
2018-10-17 13:16:22 +0800

    finish contiguous list

--- 06_ListContiguous_main.cpp
+++ 06_ListContiguous_main.cpp
@@ -7,39 +7,52 @@ void visit(char& ch){
 }
 
 int main(){
-
   List<char> l;
 
-  // visit(l.entry[0]);
-  // printf("%d",l.insert(0,'a'));
-  // visit(l.entry[0]);
+  // printf("\n");
+  // l.insert(0,'a');
+  // l.insert(0,'s');
+  // l.insert(0,'d');
+  // printf("%d\n",l.insert(5,'x'));
+  // printf("%d\n",l.insert(4,'x'));
+  // printf("%d\n",l.insert(3,'x'));
+  // l.traverse(visit);
+
+  // printf("\n");
+  // l.insert(0,'a');
+  // l.insert(0,'s');
+  // l.insert(0,'d');
+  // char ch=0;
+  // printf("%d\n",l.retrieve(-1,ch));
+  // printf("%d\n",l.retrieve(3,ch));
+  // printf("%d\n",l.retrieve(4,ch));
+  // printf("%d\n",l.retrieve(0,ch));printf("%c\n",ch);
+  // printf("%d\n",l.retrieve(1,ch));printf("%c\n",ch);
+  // printf("%d\n",l.retrieve(2,ch));printf("%c\n",ch);
+
+  // printf("\n");
+  // l.insert(0,'a');
+  // l.insert(0,'s');
+  // l.insert(0,'d');
+  // char ch=0;
+  // printf("%d\n",l.remove(-1,ch));
+  // printf("%d\n",l.remove(3,ch));
+  // printf("%d\n",l.remove(4,ch));
+  // printf("%d\n",l.remove(0,ch));l.traverse(visit);printf(" -> %c\n",ch);
+  // printf("%d\n",l.remove(0,ch));l.traverse(visit);printf(" -> %c\n",ch);
+  // printf("%d\n",l.remove(0,ch));l.traverse(visit);printf(" -> %c\n",ch);
+
   printf("\n");
   l.insert(0,'a');
   l.insert(0,'s');
   l.insert(0,'d');
-  l.insert(3,'x');
-  l.insert(3,'x');
-  l.insert(0,'d');
-  l.insert(6,'y');
-  l.insert(6,'y');
-  // printf("%d\n",l.count);
-  l.traverse(visit);
-
-  // printf("\n");
-  // l.serve();
-  // l.print();
-
-  // printf("\n");
-  // ch=0;
-  // l.retrieve(ch);
-  // printf("%c\n",ch);
-  // printf("%d\n",l.size());
-
-  // printf("\n");
-  // ch=0;
-  // l.serve_and_retrieve(ch);
-  // printf("%c\n",ch);
-  // printf("%d\n",l.size());
+  char ch=0;
+  printf("%d\n",l.remove(-1,ch));
+  printf("%d\n",l.remove(3,ch));
+  printf("%d\n",l.remove(4,ch));
+  printf("%d\n",l.remove(2,ch));l.traverse(visit);printf(" -> %c\n",ch);
+  printf("%d\n",l.remove(1,ch));l.traverse(visit);printf(" -> %c\n",ch);
+  printf("%d\n",l.remove(0,ch));l.traverse(visit);printf(" -> %c\n",ch);
 
   printf("\n");
   return 0;
